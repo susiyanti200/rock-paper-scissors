96eaf2525a06df55c0d21bb5ace605f44b140c55
susiyanti <susiyanti.g11@gmail.com>
2021-04-30 19:57:52 +0700

    Hook up game play logic to dom

--- script.js
+++ script.js
@@ -1,47 +1,76 @@
-const selection = ["rock", "paper", "scissors"];
+const win = { 0: 2, 1: 0, 2: 1 };
 
 const score = { player: 0, computer: 0 };
 
-const computerPlay = () => selection[Math.floor(Math.random() * 3)];
+const computerPlay = () => Math.floor(Math.random() * 3);
 
-const playRound = (playerSelection, computerSelection) => {
+const checkRound = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    return "Tie";
+    return "fa-meh";
   }
-  if (!selection.includes(playerSelection)) {
-    return "Wrong Selection";
+  if (win[playerSelection] === computerSelection) {
+    score["player"]++;
+    return "fa-smile";
   }
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    score["player"]++;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    score["player"]++;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    score["player"]++;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    score["computer"]++;
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  }
+  score["computer"]++;
+  return "fa-frown";
 };
 
-const game = () => {
-  for (let index = 0; index < 5; index++) {
-    const playerSelection = window.prompt("Rock, Paper or Scissors").toLowerCase();
-    const result = playRound(playerSelection, computerPlay());
-    console.log(result);
-    if (result === "Wrong Selection") {
-      index--;
+const displayScore = () => {
+  playerScore.textContent = score["player"];
+  computerScore.textContent = score["computer"];
+};
+
+const checkWinner = () => {
+  if (score["player"] == 5 || score["computer"] == 5) {
+    if (score["player"] > score["computer"]) {
+      resultIcon.classList = "fas fa-grin-stars";
+    } else {
+      resultIcon.classList = "fas fa-sad-cry";
     }
-  }
-  if (score["player"] > score["computer"]) {
-    console.log(`You Win. ${score["player"]}:${score["computer"]}`);
-  } else if (score["player"] < score["computer"]) {
-    console.log(`You Lose. ${score["player"]}:${score["computer"]}`);
-  } else {
-    console.log("It's a tie!");
+    playButton.style.display = "block";
+    playerSection.classList.remove("player-hover");
+    playing = false;
   }
 };
 
-game();
+const playRound = (e) => {
+  if (!playing) return;
+  const playerSelection = parseInt(e.currentTarget.dataset.index);
+  const computerSelection = computerPlay();
+  computerWeapon.forEach((btn) => {
+    if (parseInt(btn.dataset.index) === computerSelection) {
+      btn.classList.add("choose");
+      setTimeout(() => btn.classList.remove("choose"), 500);
+    }
+  });
+  const result = checkRound(playerSelection, computerSelection);
+  resultIcon.classList = `fas ${result}`;
+  resultIcon.style.display = "block";
+  displayScore();
+  checkWinner();
+};
+
+const startGame = () => {
+  score["player"] = 0;
+  score["computer"] = 0;
+  displayScore();
+  resultIcon.style.display = "none";
+  playButton.style.display = "none";
+  playing = true;
+  playerSection.classList.add("player-hover");
+};
+
+const playerSection = document.querySelector(".player");
+const playerWeapon = document.querySelectorAll(".player button");
+const computerWeapon = document.querySelectorAll(".computer button");
+const playButton = document.querySelector("#play");
+const resultIcon = document.querySelector(".result i");
+const playerScore = document.querySelector("#scorePlayer");
+const computerScore = document.querySelector("#scoreComputer");
+
+let playing = false;
+
+playButton.addEventListener("click", startGame);
+playerWeapon.forEach((btn) => btn.addEventListener("click", playRound));
+startGame();
